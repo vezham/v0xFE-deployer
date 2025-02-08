481216a49335f113562c7559631032862d78b12c
vigneshwaran-vtpl <vigneshwaran@vezham.com>
2025-02-08 05:52:32 +0530

    ci: skip  strict mode, error handling

--- .github/actions/changesets/checklist-release-v1/bin/main.sh
+++ .github/actions/changesets/checklist-release-v1/bin/main.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/github-actions.sh"
 
 pre_setup(){
   # Set strict mode, error handling
-  set -euo pipefail
+  # set -euo pipefail
   trap cleanup EXIT
 
   # Initialize logging
