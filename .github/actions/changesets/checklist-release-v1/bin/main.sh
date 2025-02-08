1bd19f876a435a63b4acc1535e7162d26cb4d3e8
vigneshwaran-vtpl <vigneshwaran@vezham.com>
2025-02-08 05:59:44 +0530

    Update main.sh

--- .github/actions/changesets/checklist-release-v1/bin/main.sh
+++ .github/actions/changesets/checklist-release-v1/bin/main.sh
@@ -8,6 +8,7 @@ source "$(dirname "$0")/github-actions.sh"
 pre_setup(){
   # Set strict mode, error handling
   # set -euo pipefail
+  set -eo pipefail
   trap cleanup EXIT
 
   # Initialize logging
